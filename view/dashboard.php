ee61c8b5747424bc767b70d0e23115414ac9417c
ali bouaida <alibouaida@gmail.com>
2018-04-18 17:30:04 +0200

    Comments1

--- view/dashboard.php
+++ view/dashboard.php
@@ -44,6 +44,7 @@
                                 </p>
                             </div>
                         </div>
+
                         <div class="row">
                             <strong>Tags:</strong>
                             <?php
@@ -51,8 +52,14 @@
                             $tags = getTagsByArtist($artistName);
                             displayTagsByArtist($tags);
                             ?>
+
                         </div>
                     </div>
+                    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+                    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+                    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+                    <!------ Include the above in your HEAD tag ---------->
+
                 <?php } ?>
             </div>
         </div>
