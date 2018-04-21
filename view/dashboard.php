9e90019b778784ea7400dce5883e1745cdd50298
GuiguiWeb <guillaume.douceron@free.fr>
2018-04-21 17:32:36 +0200

    Likes système

--- view/dashboard.php
+++ view/dashboard.php
@@ -37,6 +37,21 @@ include '_topbar.php'; ?>
                                             echo '<li><a href="delete.php?id=' . $music['id'] . '"><i class="fa fa-times"></i></a></li>';
                                         } ?>
                                     </ul>
+                                    <?php $_SESSION['id'] = 7; //addLike($db, $_SESSION['id'],  $music['id']);
+                                    if (getLikesByPostAndUser($db, $_SESSION['id'], $music['id'])) : ?>
+                                        <span style="color: red;">
+                                            <?php echo getNumberOfLikesByMusic($db, $music['id']); ?>
+                                            <i class="fa fa-heart" aria-hidden="true"></i>
+                                        </span>
+                                    <?php else: ?>
+                                        <form action="likes.php" method="post">
+                                            <input type='hidden' name='musicid' value='<?php echo $music['id']; ?>'>
+                                            <button type='submit' name='likeSubmit' class='btn btn-danger'><span style="color: red;"><?php echo getNumberOfLikesByMusic($db, $music['id']); ?>
+                                                    <i class="fa fa-heart-o" aria-hidden="true"></i></span>
+                                            </button>
+                                        </form>
+
+                                    <?php endif;; ?>
                                 </div>
                                 <b class="username">Posté par <?php echo $music['username']; ?></b>
                                 <h3 class="title">
@@ -74,10 +89,13 @@ include '_topbar.php'; ?>
                             if (!empty($comments)) :
                                 foreach ($comments as $row) : ?>
                                     <p><?php echo $row['message']; ?></p>
-                                    <p><small>
-                                        Message posté par <strong><?php echo $row['username']; ?></strong> le <?php $date = new Date($row['date']);
-                                        echo $date->format('l j F Y H:i'); ?>
-                                    </small></p>
+                                    <p>
+                                        <small>
+                                            Message posté par
+                                            <strong><?php echo $row['username']; ?></strong> le <?php $date = new Date($row['date']);
+                                            echo $date->format('l j F Y H:i'); ?>
+                                        </small>
+                                    </p>
                                     <hr>
 
                                 <?php endforeach; endif; ?>
